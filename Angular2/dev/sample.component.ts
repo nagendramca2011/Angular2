fd4ff7f7c6ab1fd3f34d49ff75e63d5f6c925b97
Nagendra <nagendramca2011@gmail.com>
2018-05-09 22:35:25 +0530

    manipulating template #1

--- Angular2/dev/sample.component.ts
+++ Angular2/dev/sample.component.ts
@@ -4,10 +4,16 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'sample',
     template: `
-    	This is my Sample Component
-   `
+        Hi I'm {{name}} and this is my first angular2 component! <span [class.is-awesome]="inputElement.value==='yes'" >It's Awesome!</span>
+    <br>
+    <br>
+    Is it awesome?
+    <input text="text" #inputElement (keyup)="0">
+   `,
+    styleUrls:['src/css/sample.css']
 
 })
 export class SampleComponent {
-
+    name = 'Nagendra';
 }
+//[class.is-awesome] is a class directive 
