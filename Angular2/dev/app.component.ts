f413558ed2901fa688a6aa2c6a9e115aed80f43a
Nagendra <nagendramca2011@gmail.com>
2018-05-09 21:56:43 +0530

    added  multiple components

--- Angular2/dev/app.component.ts
+++ Angular2/dev/app.component.ts
@@ -1,8 +1,15 @@
+
 import {Component} from 'angular2/core';
+import {SampleComponent} from './sample.component';
 
 @Component({
     selector: 'app',
-    template: 'Hello World!',
+    template: `
+    	<h1> This is my app component </h1>  
+        <sample></sample>      
+    `,
+    directives:[SampleComponent]
+
 })
 export class AppComponent {
 
---
+++ Angular2/dev/sample.component.ts
@@ -0,0 +1,13 @@
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'sample',
+    template: `
+    	This is my Sample Component
+   `
+
+})
+export class SampleComponent {
+
+}
